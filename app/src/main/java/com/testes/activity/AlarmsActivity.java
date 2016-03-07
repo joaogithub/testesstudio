2668016bb3bd88e1a39f1b3bcfec668d710ab91c
João Amaro <joaom.amaro.silva@gmail.com>
2016-03-07 15:56:41 +0000

    formatting commit

--- app/src/main/java/com/testes/activity/AlarmsActivity.java
+++ app/src/main/java/com/testes/activity/AlarmsActivity.java
@@ -15,87 +15,87 @@ import android.support.v7.app.ActionBarActivity;
 
 import com.testes.android.R;
 
-public class AlarmsActivity extends ActionBarActivity{
+public class AlarmsActivity extends ActionBarActivity {
 
-	private String buttonText= "";
-	private long timeInMs;
-	private int imagen=0;
+    private String buttonText = "";
+    private long timeInMs;
+    private int imagen = 0;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		setContentView(R.layout.activity_dog);
+        setContentView(R.layout.activity_dog);
 
 //		setAlarm();
-		setOneDayAlarm();
+        setOneDayAlarm();
 
-	}
+    }
 
-	public void setOneDayAlarm(){
-		Calendar c = Calendar.getInstance();
-		c.set(2014, 9, 24);
-		c.set(Calendar.HOUR_OF_DAY, 12);
-		c.set(Calendar.MINUTE, 13);
-		c.set(Calendar.SECOND, 0);
-		Intent intentservice = new Intent(this, FirstActivity.class);
+    public void setOneDayAlarm() {
+        Calendar c = Calendar.getInstance();
+        c.set(2014, 9, 24);
+        c.set(Calendar.HOUR_OF_DAY, 12);
+        c.set(Calendar.MINUTE, 13);
+        c.set(Calendar.SECOND, 0);
+        Intent intentservice = new Intent(this, FirstActivity.class);
 
-		//create a pending intent to be called at 6 AM
-		PendingIntent pIntent = PendingIntent.getActivity(this, 0, intentservice, PendingIntent.FLAG_UPDATE_CURRENT);
-		//schedule time for pending intent, and set the interval to day so that this event will repeat at the selected time every day
+        //create a pending intent to be called at 6 AM
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intentservice, PendingIntent.FLAG_UPDATE_CURRENT);
+        //schedule time for pending intent, and set the interval to day so that this event will repeat at the selected time every day
 
-		AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
+        AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
 
-		//schedule time for pending intent, and set the interval to day so that this event will repeat at the selected time every day
-		am.setRepeating(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), 24*3600*1000, pIntent);
-	}
+        //schedule time for pending intent, and set the interval to day so that this event will repeat at the selected time every day
+        am.setRepeating(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), 24 * 3600 * 1000, pIntent);
+    }
 
-	public void setAlarm(){
+    public void setAlarm() {
 
-		int hour = 19;
-		int minute = 00;
-		String myTime = String.valueOf(hour) + ":" + String.valueOf(minute);
+        int hour = 19;
+        int minute = 00;
+        String myTime = String.valueOf(hour) + ":" + String.valueOf(minute);
 
-		Date date = null;
+        Date date = null;
 
-		// today at your defined time Calendar    
-		Calendar customCalendar = new GregorianCalendar();
-		// set hours and minutes
-		customCalendar.set(Calendar.HOUR_OF_DAY, hour);
-		customCalendar.set(Calendar.MINUTE, minute);
-		customCalendar.set(Calendar.SECOND, 0);
-		customCalendar.set(Calendar.MILLISECOND, 0);
+        // today at your defined time Calendar
+        Calendar customCalendar = new GregorianCalendar();
+        // set hours and minutes
+        customCalendar.set(Calendar.HOUR_OF_DAY, hour);
+        customCalendar.set(Calendar.MINUTE, minute);
+        customCalendar.set(Calendar.SECOND, 0);
+        customCalendar.set(Calendar.MILLISECOND, 0);
 
-		Date customDate = customCalendar.getTime();
+        Date customDate = customCalendar.getTime();
 
-		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm", Locale.getDefault());
-		try {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm", Locale.getDefault());
+        try {
 
-			date = sdf.parse(myTime);
+            date = sdf.parse(myTime);
 
-		} catch (ParseException e) {
+        } catch (ParseException e) {
 
-			e.printStackTrace();
-		} catch (java.text.ParseException e) {
-			e.printStackTrace();
-		}
+            e.printStackTrace();
+        } catch (java.text.ParseException e) {
+            e.printStackTrace();
+        }
 
-		if (date != null) {
-			timeInMs = customDate.getTime();
-		}
+        if (date != null) {
+            timeInMs = customDate.getTime();
+        }
 
-		
-		Intent intent = new Intent();
-		
-		switch(imagen){
-		
-		}
-		
-		intent = new Intent(this, FirstActivity.class);
-		PendingIntent actionIntent = PendingIntent.getActivity(this, 0,intent, Intent.FLAG_ACTIVITY_NEW_TASK);
 
-		AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-		alarmManager.set(AlarmManager.RTC_WAKEUP, timeInMs, actionIntent);
-	}
+        Intent intent = new Intent();
+
+        switch (imagen) {
+
+        }
+
+        intent = new Intent(this, FirstActivity.class);
+        PendingIntent actionIntent = PendingIntent.getActivity(this, 0, intent, Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, timeInMs, actionIntent);
+    }
 
 }
